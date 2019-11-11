739f5ba024c7013bd05c6f7534d17d7eacf1b5d3
SaumilShah66 <shah.saumil66@gmail.com>
2019-11-11 09:18:18 -0500

    [talker.cpp]Comment added before node initialization

--- src/talker.cpp
+++ src/talker.cpp
@@ -50,9 +50,19 @@
  * @return 1 When fatal error stream is reached
  */
 int main(int argc, char **argv) {
-
+  /**
+   * The ros::init() function needs to see argc and argv so that it can perform
+   * any ROS arguments and name remapping that were provided at the command line.
+   * For programmatic remappings you can use a different version of init() which takes
+   * remappings directly, but for most command-line programs, passing argc and argv is
+   * the easiest way to do it.  The third argument to init() is the name of the node.
+   *
+   * You must call one of the versions of ros::init() before using any other
+   * part of the ROS system.
+   */
   ros::init(argc, argv, "talker");
-
+   
+  
   /// Inializing tf broadcaster
   tf::TransformBroadcaster br;
   tf::Transform transform;
@@ -65,16 +75,7 @@ int main(int argc, char **argv) {
   int frequencyVal = 10;  // Frequency in Hz
   std::string name = "Saumil";  // Default name
   bool status = true;
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line.
-   * For programmatic remappings you can use a different version of init() which takes
-   * remappings directly, but for most command-line programs, passing argc and argv is
-   * the easiest way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
+ 
 
   std::stringstream sss;
   std_msgs::String msgs;
